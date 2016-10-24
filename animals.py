74796bad3b797b102309d705f0078fce60914e19
Anayatc <anayatc@sky.com>
2016-10-24 20:35:26 +0100

    New

--- animals.py
+++ animals.py
@@ -1,7 +1,11 @@
+from __future__ import print_function
+
+
 def list_animals(animals):
     list = ''
     for i in animals:
         list += str(i) + 1 + '. ' + animals[i] + '\n'
     return list
 
-print list_animals(['dog', 'cat', 'elephant'])
+
+print(list_animals(['dog', 'cat', 'elephant']))
