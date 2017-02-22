d541f2000cb78cfe3c50e7d99bd51fa7a79355e2
Anayat <anayatc@gmail.com>
2017-02-22 11:35:43 +0000

    New

---
+++ Investment_Return-BETA.py
@@ -0,0 +1,10 @@
+def calc_investment(investors, amount):
+    return round(investors * amount * 0.0025)
+
+'''
+Description:
+
+Let's say you are starting an online investment firm. If you can collect 0.25% of the total amount invested by
+your clients, how much would you make a year, assuming you have a certain amount of investors investing the same
+amount of dollars each? Round your solution to the nearest dollar.
+'''
