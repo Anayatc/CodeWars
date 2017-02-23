a8b9a682b211dd6a53fed83434fa5bff443eeda9
Anayat <anayatc@gmail.com>
2017-02-23 11:43:21 +0000

    New

---
+++ The_Office_III_Broken_Photocopier-7KYU.py
@@ -0,0 +1,14 @@
+def broken(inp):
+    return ''.join(['0' if i == '1' else '1' for i in inp])
+
+print(broken('1101'))
+
+'''
+Description:
+
+The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+
+Given a string of binary, return the version the photocopier gives you as a string.
+'''
