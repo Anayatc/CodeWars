244544e0c75c3fe1358e8606ef59e9920aec1c50
Anayatc <Sincity9490>
2017-03-30 09:27:14 +0100

    New

--- Equal_Sides_Of_An_Array-6KYU.py
+++ Equal_Sides_Of_An_Array-6KYU.py
@@ -1,6 +1,8 @@
 def find_even_index(arr):
-    for i in arr:
-
+    for i in range(len(arr)):
+        if sum(arr[:i]) == sum(arr[i+1:]):
+            return i
+    return -1
 
 print(find_even_index([1,2,3,4,3,2,1]))
 
