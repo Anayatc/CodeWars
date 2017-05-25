0ea41d392e40076c2e8ddbc7884109b0f16cf0d8
anayatc@gmail.com <anayatc@sky.com>
2017-05-25 22:44:17 +0100

    removed description

--- (8KYU) Are They Opposite/Are_They_Opposite-8KYU.py
+++ (8KYU) Are They Opposite/Are_They_Opposite-8KYU.py
@@ -4,7 +4,8 @@ def is_opposite(s1, s2):
 print(is_opposite('', ''))
 
 '''
-Before Refactoring
+Before Refactoring:
+
 def is_opposite(s1, s2):
     t1 = s1.lower()
     t2 = s2.lower()
@@ -15,24 +16,4 @@ def is_opposite(s1, s2):
         if (t1[i] == t2[i]) and ((s1[i].islower() == s2[i].isupper()) or (s1[i].isupper() == s2[i].islower())):
             count[i] = True
     return all(count)
-'''
-
-'''
-Description:
-
-They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it series #1:
-Are they opposite?
-
-Task
-
-Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. Note: The result should be a boolean value, instead of a string.
-The opposite means: All letters of the two strings are the same, but the case is opposite. you can assume that the string only contains letters or it's a empty string
-
-Some examples:
-
-isOpposite("ab","AB") should return true;
-isOpposite("aB","Ab") should return true;
-isOpposite("aBcd","AbCD") should return true;
-isOpposite("AB","Ab") should return false;
-isOpposite("","") should return false;
 '''
