bddae758ce8bbefc217172df159617268d11da4f
anayatc@gmail.com <anayatc@sky.com>
2017-05-25 21:41:09 +0100

    removed description

--- (6KYU) Alternating Loops/Alternating_Loops-6KYU.py
+++ (6KYU) Alternating Loops/Alternating_Loops-6KYU.py
@@ -1,21 +1,6 @@
 import itertools
 
+
 def combine(*args):
     return filter(lambda x: x is not None, itertools.chain.from_iterable(itertools.izip_longest(*args)))
-
-'''
-Description:
-
-Write
-
-function combine()
-that combines arrays by alternatingly taking elements passed to it.
-
-E.g
-
-combine(['a', 'b', 'c'], [1, 2, 3]) == ['a', 1, 'b', 2, 'c', 3]
-combine(['a', 'b', 'c'], [1, 2, 3, 4, 5]) == ['a', 1, 'b', 2, 'c', 3, 4, 5]
-combine(['a', 'b', 'c'], [1, 2, 3, 4, 5], [6, 7], [8]) == ['a', 1, 6, 8, 'b', 2, 7, 'c', 3, 4, 5]
-Arrays can have different lengths.
-'''
 
