24d0419b83da7b7c67812d8666c9b28e9ec25646
anayatc@gmail.com <anayatc@sky.com>
2017-06-02 01:48:26 +0100

    updated directory

--- (7KYU) Binary Addition/Binary_Addition-7KYU.py
+++ (7KYU) Binary Addition/Binary_Addition-7KYU.py
@@ -11,12 +11,3 @@ def add_binary(a, b):
 
 
 print add_binary(6, 6)
-
-'''
-Description:
-
-Implement a function that adds two numbers together and returns their sum in binary.
-The conversion can be done before, or after the addition.
-
-The binary number returned should be a string.
-'''
